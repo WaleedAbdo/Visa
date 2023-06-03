cabbdfd0089e4cd91e956264491e8873b6eef821
wedowell2022 <waleedAbdo2022@outlook.com>
2023-06-03 17:45:59 +0300

    init

---
+++ Visa.BL/Mapper/DomainProfile.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+
+using Visa.BL.Models;
+using Visa.DAL.Entity;
+
+namespace Visa.BL.Mapper
+{
+    public class DomainProfile : Profile
+    {
+
+        public DomainProfile()
+        {
+            CreateMap<HeaderVM, Header>().ReverseMap();
+
+            CreateMap<ServicesVM, Services>().ReverseMap();
+
+            CreateMap<AboutVM, About>().ReverseMap();
+
+            CreateMap<ContactVM, Contact>().ReverseMap();
+
+            CreateMap<StepsVM, Steps>().ReverseMap();
+
+            CreateMap<HomeTestimonialsVM, HomeTestimonials>().ReverseMap();
+
+            CreateMap<ContactBannerVM, ContactBanner>().ReverseMap();
+
+            CreateMap<LandingVm, Landing>().ReverseMap();
+
+            CreateMap<FaQuestionVm, FaQuestion>().ReverseMap();
+
+            CreateMap<StampedVisaVM, StampedVisa>().ReverseMap();
+
+            CreateMap<LookUpValueVM, LookUpValue>().ReverseMap();
+            CreateMap<Testimonials, TestimonialsVM>().ReverseMap();
+        }
+
+    }
+}
---
+++ Visa.DAL/Entity/Landing.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visa.DAL.Entity
+{
+    [Table("Landing")]
+    public class Landing
+    {
+        public int Id { get; set; }
+        [Required]
+
+        public string TitleAr { get; set; }
+        [Required]
+
+        public string TitleEn { get; set; }
+        [Required]
+
+        public string DescriptionAr { get; set; }
+        [Required]
+
+        public string DescriptionEn { get; set; }
+        [Required]
+
+        public string ImageName { get; set; }
+        [Required]
+
+        public string VideoTitleAr { get; set; }
+        [Required]
+
+        public string VideoTitleEn { get; set; }
+
+        public bool? IsDeleted { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+
+
+        public ICollection<FaQuestion>? FaQuestion { get; set; }
+        public ICollection<StampedVisa>? StampedVisa { get; set; }
+    }
+}
